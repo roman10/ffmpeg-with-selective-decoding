d7548ea6d3af524e162cc2af68f3675dba7a597a
roman10 <liuf0005@gmail.com>
2011-10-19 23:54:20 -0400

    add video_conversion_command.sh

---
+++ video_conversion_command.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/ffmpeg -i "/home/roman10/Desktop/master/ffmpeg-with-selective-decoding/2.3gp" -vcodec mpeg4 -r 25.00 -s 720x480  -aspect 3:2 -b 4000k "/home/roman10/Desktop/master/ffmpeg-with-selective-decoding/2.mp4"
+/usr/local/bin/ffmpeg -i h1.mp4 -vcodec mpeg4 -r 30 -s 1280x720 -aspect 60:45 -b 10000k h1_1280_720.mp4
